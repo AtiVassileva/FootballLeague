6e4789b733a00dc0e8311656db8ebc7e437f5b96
Ati <ativassileva@gmail.com>
2023-02-12 21:56:32 +0200

    Added teams controller tests for get all

---
+++ src/FootballLeague.Tests/Controllers/TeamsControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using FootballLeague.API.Controllers;
+using FootballLeague.API.Services.Contracts;
+using FootballLeague.Models.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+
+namespace FootballLeague.Tests.Controllers
+{
+    [TestFixture]
+    public class TeamsControllerTests
+    {
+        [Test]
+        public async Task GetAllShouldReturnOkResponseWithData()
+        {
+            var mockService = new Mock<ITeamsService>();
+            mockService.Setup(x => x.GetAllTeams())
+                .ReturnsAsync(new List<TeamResponseModel>(1));
+
+            var controller = new TeamsController(mockService.Object);
+            var result = await controller.GetAll();
+
+            Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        }
+
+        [Test]
+        public async Task GetAllShouldReturnBadRequestWithoutData()
+        {
+            var mockService = new Mock<ITeamsService>();
+            mockService.Setup(x => x.GetAllTeams())
+                .ThrowsAsync(new ArgumentException());
+
+            var controller = new TeamsController(mockService.Object);
+            var result = await controller.GetAll();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.InstanceOf<ObjectResult>());
+                Assert.That(((ObjectResult)result).StatusCode, Is.EqualTo(StatusCodes.Status400BadRequest));
+            });
+        }
+        
+        [Test]
+        public async Task GetAllShouldReturnInternalServerErrorWhenErrorOccurs()
+        {
+            var mockService = new Mock<ITeamsService>();
+            mockService.Setup(x => x.GetAllTeams())
+                .ThrowsAsync(new FormatException());
+
+            var controller = new TeamsController(mockService.Object);
+            var result = await controller.GetAll();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.InstanceOf<ObjectResult>());
+                Assert.That(((ObjectResult)result).StatusCode, Is.EqualTo(StatusCodes.Status500InternalServerError));
+            });
+        }
+    }
+}
